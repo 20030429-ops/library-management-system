253b6c019231e0f7f7c851dd376b61247f132eb6
20030429-ops <20030429@students.koi.edu.au>
2026-05-20 12:48:52 +1000

    Add files via upload

---
+++ BorrowController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class BorrowController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(object borrow)
+        {
+            ViewBag.Message = "Borrowing Transaction Completed Successfully";
+
+            return View();
+        }
+    }
+}
